8b0eb323e7d13d0b0ea9a06c9078914115e3f435
toledanogus <toledanogu@gmail.com>
2023-03-25 13:39:46 -0600

    entermok1

--- src/MultiplicacionesApp.jsx
+++ src/MultiplicacionesApp.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 let arr = [];
 let fin = 'Tabla del ';
-const tablasM = [1,2,3,4,5,6,7,8,9];
+const tablasM = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export const MultiplicacionesApp = () => {
 
@@ -12,14 +12,14 @@ export const MultiplicacionesApp = () => {
   const [puntaje, setPuntaje] = useState(0);
   const [visible, setVisible] = useState(true);
   const inputRef = useRef(null)
-  
 
-useEffect(() => {
-  if (puntaje % 20 === 0 && puntaje != 0 && tabla<=9) {
-    setTabla(tabla+1);
-    setVisible (true);
-  }
-}, [puntaje])
+
+  useEffect(() => {
+    if (puntaje % 20 === 0 && puntaje != 0 && tabla <= 9) {
+      setTabla(tabla + 1);
+      setVisible(true);
+    }
+  }, [puntaje])
 
   useEffect(() => {
     if (puntaje % 20 === 0 && puntaje != 0 && tabla <= 9) {
@@ -53,56 +53,56 @@ useEffect(() => {
       setPuntaje(puntaje + 1);
       resultadoU.value = "";
       arr.unshift(multiplicador);
-      setMultiplicador( Math.floor(Math.random()*10)+1);
-      }
+      setMultiplicador(Math.floor(Math.random() * 10) + 1);
+    }
 
   }
 
   const calcular2 = (e) => {
-if (e.key === 'Enter' || e.key === 'Accept') {
-  
-}
-    const resultadoU = document.querySelector("#resultado");
-    const resultado = tabla * multiplicador;
-    if (resultadoU.value == resultado) {
-      setPuntaje(puntaje + 1);
-      resultadoU.value = "";
-      arr.unshift(multiplicador);
-      setMultiplicador( Math.floor(Math.random()*10)+1);
+    if (e.key === 'Enter' || e.key === 'Accept') {
+      const resultadoU = document.querySelector("#resultado");
+      const resultado = tabla * multiplicador;
+      if (resultadoU.value == resultado) {
+        setPuntaje(puntaje + 1);
+        resultadoU.value = "";
+        arr.unshift(multiplicador);
+        setMultiplicador(Math.floor(Math.random() * 10) + 1);
       }
+    }
   }
+
 
   return (
     <>
-    {
-      visible ? (
-    <>
-    <h1>Tablas de multiplicar</h1>
-    <div>{tabla} <span>x</span>  {multiplicador} = <input type="number" id="resultado" ref={inputRef}/> </div>
-    <button onClick={calcular} onKeyDown={calcular2}>Verificar</button>
-    <div>Puntaje = {puntaje}</div>
-    <br />
-    <div>Elige la tabla que desees: </div>
-    <select name="tablas" id="tablas">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-    </select>
-    <button onClick={getTable}>Aplicar</button>
-    </>
-    ):
-    <>
-    <h1>{fin} {tabla}</h1>
-    {/* <h2>Tu resultado final fue de: {puntaje}</h2> */}
-    </>
-    
-    }
+      {
+        visible ? (
+          <>
+            <h1>Tablas de multiplicar</h1>
+            <div>{tabla} <span>x</span>  {multiplicador} = <input type="number" id="resultado" ref={inputRef} /> </div>
+            <button onClick={calcular} onKeyDown={calcular2}>Verificar</button>
+            <div>Puntaje = {puntaje}</div>
+            <br />
+            <div>Elige la tabla que desees: </div>
+            <select name="tablas" id="tablas">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+            </select>
+            <button onClick={getTable}>Aplicar</button>
+          </>
+        ) :
+          <>
+            <h1>{fin} {tabla}</h1>
+            {/* <h2>Tu resultado final fue de: {puntaje}</h2> */}
+          </>
+
+      }
     </>
   )
 }
